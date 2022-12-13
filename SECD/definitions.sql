52cef0b9c29c7fda1bc7c30e46b89be772cf03f4
j-w-moebius <johann.moebius@gmx.net>
2022-12-13 15:43:58 +0100

    represent environment within recursive CTE

--- SECD/definitions.sql
+++ SECD/definitions.sql
@@ -1,5 +1,5 @@
-DROP TYPE IF EXISTS term,var,primitive,env,closure,val,stack,directive,control,frame,dump,term_type,lam,app CASCADE;
-DROP TABLE IF EXISTS environments, terms;
+DROP TYPE IF EXISTS term,var,primitive,env,closure,val,stack,directive,control,frame,dump,lam,app,rule,machine_state,env_entry CASCADE;
+DROP TABLE IF EXISTS terms;
 
 --CREATE DOMAIN term AS jsonb;
 CREATE DOMAIN term AS integer;
@@ -16,6 +16,11 @@ CREATE DOMAIN dump AS frame[];
 
 CREATE TYPE lam AS (ide var, body term);
 CREATE TYPE app AS (fun term, arg term);
+
+CREATE TYPE rule AS ENUM('1', '2', '3', '4', '5', '6', '7');
+
+CREATE TYPE machine_state AS (s stack, e env, c control, d dump);
+CREATE TYPE env_entry AS (id env, name var, val val);
 
 CREATE TABLE terms (id integer GENERATED ALWAYS AS IDENTITY, lit int, var var, lam lam, app app);
 
@@ -60,58 +65,5 @@ $$
 $$
 LANGUAGE SQL VOLATILE;
 
-CREATE TABLE environments(id env, name var, def val);
-ALTER TABLE environments
-  ADD PRIMARY KEY (id, name);
-
--- look up identifier ide in environment env
--- return corresponding value or null if ide not defined in env
-CREATE FUNCTION lookup(e env, ide var) RETURNS val AS
-$$
-  SELECT t.def
-  FROM environments AS t
-  WHERE t.id = e
-    AND t.name = ide
-$$
-LANGUAGE SQL VOLATILE;
-
--- extend an environment env's bindings by (ide -> v)
-CREATE FUNCTION extend(e env, ide var, v val) RETURNS env AS
-$$
-  INSERT INTO environments VALUES (e, ide, v)
-    ON CONFLICT (id, name) DO UPDATE
-      SET def = v
-  RETURNING e
-$$
-LANGUAGE SQL VOLATILE;
-
--- return new empty env
-CREATE FUNCTION empty_env() RETURNS env AS 
-$$
-  SELECT nextval('env_keys')
-$$
-LANGUAGE SQL VOLATILE;
-
--- delete env old and return new
-CREATE FUNCTION replace_env(old env, new env) RETURNS env AS 
-$$
-  DELETE FROM environments AS t
-    WHERE t.id = old;
-  SELECT new
-$$
-LANGUAGE SQL VOLATILE;
-
--- create copy of env e
-CREATE FUNCTION copy_env(e env) RETURNS env AS
-$$
-  WITH fresh_env AS 
-    (SELECT empty_env()),
-  _ AS                   -- RETURNING doesn't work with empty environments
-    (INSERT INTO environments
-      (SELECT f.id, t.name, t.def
-       FROM environments AS t, fresh_env AS f(id)
-       WHERE t.id = e))
-  SELECT f.id
-  FROM fresh_env AS f(id)
-$$
-LANGUAGE SQL VOLATILE;
+DROP TYPE IF EXISTS rec_type;
+CREATE TYPE rec_type AS (finished boolean, ms machine_state, e env_entry);
--- SECD/secd.sql
+++ SECD/secd.sql
@@ -1,115 +1,178 @@
 \i definitions.sql
 
-DROP TYPE IF EXISTS machine_state;
-CREATE TYPE machine_state AS (s stack, e env, c control, d dump, finished boolean);
-
 -- evaluate a lambda term t using an SECD machine
-CREATE FUNCTION evaluate(t term) RETURNS val AS
+CREATE OR REPLACE FUNCTION evaluate(t term) RETURNS val AS
 $$
-  WITH RECURSIVE machine_states(s,e,c,d,finished) AS (
+  WITH RECURSIVE r(finished, ms, e) AS (
   
-    SELECT array[]::stack, 
-           empty_env(), 
-           array[row(t, null)]::control, 
-           array[]::dump, 
-           false
+    SELECT 
+      false,
+      row(array[]::stack, 
+          nextval('env_keys')::env, 
+          array[row(t, null)]::control, 
+          array[]::dump)::machine_state,
+      null::env_entry
     
       UNION ALL
-      
-    SELECT step.*
-    FROM machine_states AS ms LEFT OUTER JOIN terms AS t
-            ON ms.c[1].t = t.id,
-    LATERAL (
-    
-      --1. Terminate computation
-      SELECT ms.s, 
-             ms.e, 
-             ms.c, 
-             ms.d, 
-             true
-      WHERE NOT ms.finished 
-        AND cardinality(ms.s) = 1 
-        AND cardinality(ms.c) = 0 
-        AND cardinality(ms.d) = 0
-      
-        UNION ALL
+
+    (WITH 
+      r AS (TABLE r),              -- non-linear recursion hack
+      machine(s,e,c,d) AS (
+        SELECT (r.ms).*
+        FROM r
+        WHERE r.ms IS NOT NULL
+      ),
+
+      environment(id,name,val) AS (
+        SELECT (r.e).*
+        FROM r
+        WHERE r.e IS NOT NULL
+      ),
+
+      term(lit,var,lam,app) AS (
+        SELECT t.lit, t.var, t.lam, t.app
+        FROM machine AS ms, terms AS t
+        WHERE ms.c[1].t = t.id
+      ),
+
+      step(r,s,e,c,d,id,name,val) AS (
+        --1. Terminate computation
+        SELECT '1'::rule,
+               ms.s, 
+               ms.e, 
+               ms.c, 
+               ms.d, 
+               null::env,null::var,null::val
+        FROM machine AS ms
+        WHERE cardinality(ms.s) = 1 
+          AND cardinality(ms.c) = 0 
+          AND cardinality(ms.d) = 0
         
-      --2. Return from function call
-      SELECT (ms.s || d.s)::stack, 
-             replace_env(ms.e, d.e), 
-             d.c, 
-             ms.d[2:]::dump, 
-             false
-      FROM (SELECT ms.d[1].*) AS d(s,e,c)
-      WHERE NOT ms.finished 
-        AND cardinality(ms.s) = 1 
-        AND cardinality(ms.c) = 0
-      
-        UNION ALL
+          UNION ALL
+          
+        --2. Return from function call
+        SELECT '2'::rule,
+              (ms.s || d.s)::stack, 
+              d.e, 
+              d.c, 
+              ms.d[2:]::dump, 
+              ms.e,null::var,null
+        FROM machine AS ms,
+        LATERAL (SELECT ms.d[1].*) AS d(s,e,c)
+        WHERE cardinality(ms.s) = 1 
+          AND cardinality(ms.c) = 0
         
-      --3. Push literal onto stack
-      SELECT (array[row(null,t.lit)]::stack || ms.s)::stack, 
-             ms.e, 
-             ms.c[2:]::control, 
-             ms.d, 
-             false
-      WHERE NOT ms.finished 
-        AND t.lit IS NOT NULL
-      
-        UNION ALL
+          UNION ALL
+          
+        --3. Push literal onto stack
+        SELECT '3'::rule,
+              (array[row(null,t.lit)]::stack || ms.s)::stack, 
+              ms.e, 
+              ms.c[2:]::control, 
+              ms.d, 
+              null,null::var,null
+        FROM machine AS ms,
+             term AS t
+        WHERE t.lit IS NOT NULL
+
+           UNION ALL
+          
+        --4. Push variable value onto stack
+        SELECT '4'::rule,
+               (array[(SELECT e.val
+                       FROM environment AS e
+                       WHERE e.id = ms.e AND e.name = t.var
+                       )] || ms.s)::stack, 
+                ms.e, 
+                ms.c[2:]::control, 
+                ms.d, 
+                null,null::var,null
+        FROM machine AS ms,
+             term AS t
+        WHERE t.var IS NOT NULL
+          
+          UNION ALL
+          
+        --5. Push lambda abstraction onto stack as closure
+        SELECT '5'::rule,
+              (array[row(row(lam.var, lam.body, ms.e),null)]::stack || ms.s)::stack, 
+              ms.e, 
+              ms.c[2:]::control, 
+              ms.d, 
+              null, null::var,null
+        FROM machine AS ms,
+             term AS t,
+        LATERAL (SELECT (t.lam).*) AS lam(var, body)
+        WHERE t.lam IS NOT NULL
         
-      --4. Push variable value onto stack
-      SELECT (array[lookup(ms.e,t.var)] || ms.s)::stack, 
-             ms.e, 
-             ms.c[2:]::control, 
-             ms.d, 
-             false
-      WHERE NOT ms.finished 
-        AND t.var IS NOT NULL
-      
-        UNION ALL
+          UNION ALL
         
-      --5. Push lambda abstraction onto stack as closure
-      SELECT (array[row(row(lam.var, lam.body, copy_env(ms.e)),null)]::stack || ms.s)::stack, 
-             ms.e, 
-             ms.c[2:]::control, 
-             ms.d, 
-             false
-      FROM (SELECT (t.lam).*) AS lam(var, body)
-      WHERE NOT ms.finished  
-        AND t.lam IS NOT NULL
-      
-        UNION ALL
-      
-      --6. Handle function application
-      SELECT ms.s, 
-             ms.e, 
-             (array[row(app.arg,null), row(app.fun,null), row(null, 'apply')]::control || ms.c[2:])::control, 
-             ms.d, 
-             false
-      FROM (SELECT (t.app).*) AS app(fun, arg)
-      WHERE NOT ms.finished  
-        AND t.app IS NOT NULL
-      
-        UNION ALL
+        --6. Handle function application
+        SELECT '6'::rule,
+              ms.s, 
+              ms.e, 
+              (array[row(app.arg,null), row(app.fun,null), row(null, 'apply')]::control || ms.c[2:])::control, 
+              ms.d, 
+              null,null::var,null
+        FROM machine AS ms,
+             term AS t,
+        LATERAL (SELECT (t.app).*) AS app(fun, arg)
+        WHERE t.app IS NOT NULL
         
-      --7. Apply function
-      SELECT array[]::stack, 
-             extend(closure.e, closure.v, arg), 
-             array[row(closure.t,null)]::control, 
-             (array[row(ms.s[3:],ms.e,ms.c[2:])]::dump || ms.d)::dump, 
-             false
-      FROM (SELECT ms.s[1].c.*) AS closure(v,t,e), 
-           (SELECT ms.s[2]) AS _(arg), 
-           (SELECT ms.c[1].*) AS c(_,p)
-      WHERE NOT ms.finished 
-        AND c.p = 'apply'
+          UNION ALL
+          
+        --7. Apply function
+        SELECT '7'::rule,
+              array[]::stack, 
+              nextval('env_keys')::env, 
+              array[row(closure.t,null)]::control, 
+              (array[row(ms.s[3:],ms.e,ms.c[2:])]::dump || ms.d)::dump, 
+              closure.e, closure.v, arg
+        FROM machine AS ms,
+        LATERAL (SELECT ms.s[1].c.*) AS closure(v,t,e), 
+        LATERAL (SELECT ms.s[2]) AS _(arg), 
+        LATERAL (SELECT ms.c[1].*) AS c(_,p)
+        WHERE c.p = 'apply'
+      ),
+      new_envs(id,name,val) AS (
+        -- use old env
+        SELECT e.*
+        FROM step AS s, environment AS e
+        WHERE s.r >= '2' -- rules 2,3,4,5,6,7
         
-    ) AS step(s,e,c,d,finished)  
+          UNION ALL
+
+         -- copy old env and extend it
+        SELECT s.e, s.name, s.val
+        FROM step AS s
+        WHERE s.r = '7'
+
+          UNION ALL
+        
+        SELECT s.e, e.name, e.val
+        FROM step AS s, environment AS e
+        WHERE s.r = '7'
+          AND e.id = s.id
+          AND e.name <> s.name
+      )
+
+      SELECT CASE s.r WHEN '1' THEN true ELSE false END CASE, 
+             row(s.s, s.e, s.c, s.d)::machine_state, 
+             null::env_entry
+      FROM r, step AS s
+      WHERE NOT r.finished
+
+        UNION ALL
+
+      SELECT null::boolean,
+             null::machine_state, 
+             e::env_entry
+      FROM new_envs AS e
+    )
   )
-  SELECT s[1]
-  FROM machine_states AS ms(s,_,_,_,finished)
-  WHERE finished
+  SELECT (r.ms).s[1]
+  FROM r
+  WHERE r.finished
 $$ LANGUAGE SQL VOLATILE;
 
 DROP TABLE IF EXISTS input_terms;
@@ -118,14 +181,15 @@ CREATE TABLE input_terms (t jsonb);
 \copy input_terms FROM '../term_input.json';
 
 SELECT r.*
-FROM input_terms AS _(t), evaluate(load_term(t)) AS r;
+FROM input_terms AS _(t), load_term(t) AS __(id), evaluate(id) AS r;
 
-/*
+----------------------------------------------------------------------------------
+--test
+
 DROP TABLE IF EXISTS test;
-CREATE TABLE test (s stack, e env, c control, d dump, finished boolean);
-INSERT INTO test VALUES (array[]::stack, 
-           empty_env(), 
+CREATE TABLE test (ms machine_state, e env_entry);
+INSERT INTO test VALUES (row(array[]::stack, 
+           nextval('env_keys')::env, 
            array[row(1, null)]::control, 
-           array[]::dump, 
-           false);
-*/
+           array[]::dump)::machine_state,
+           null::env_entry);
