885a8ae01463ed23c83d128b9acb94cd1de7ee21
j-w-moebius <johann.moebius@gmx.net>
2023-01-11 10:18:57 +0100

    remove unused type definitions

--- DuckDB/definitions.sql
+++ DuckDB/definitions.sql
@@ -1,51 +1,19 @@
 DROP TABLE IF EXISTS terms;
 
-DROP TYPE IF EXISTS term;
-DROP TYPE IF EXISTS var;
 DROP TYPE IF EXISTS primitive;
-DROP TYPE IF EXISTS env;
-DROP TYPE IF EXISTS closure;
-DROP TYPE IF EXISTS val;
-DROP TYPE IF EXISTS stack;
-DROP TYPE IF EXISTS directive;
-DROP TYPE IF EXISTS control;
-DROP TYPE IF EXISTS frame;
-DROP TYPE IF EXISTS dump;
-DROP TYPE IF EXISTS lam;
-DROP TYPE IF EXISTS app;
-DROP TYPE IF EXISTS rule;
 DROP TYPE IF EXISTS machine_state;
-DROP TYPE IF EXISTS env_entry;
-DROP TYPE IF EXISTS cte_type;
+DROP TYPE IF EXISTS rule;
 
-CREATE TYPE term AS integer;
-CREATE TYPE var  AS text;
 CREATE TYPE primitive AS ENUM('apply');
-CREATE TYPE env AS int;
-CREATE TYPE closure AS STRUCT(v var, t term, e env);    -- Closure = (Var, Term, Env)
-CREATE TYPE val AS UNION(c closure, n int);            -- Val = Closure | Int
-CREATE TYPE stack AS val[];
-CREATE TYPE directive AS UNION(t term, p primitive);   -- Directive = Term | Primitive
-CREATE TYPE control AS directive[];
-CREATE TYPE frame AS STRUCT(s stack, e env, c control); -- Frame = (Stack, Env, Control)
-CREATE TYPE dump AS frame[]; 
-
-CREATE TYPE lam AS STRUCT(ide var, body term);
-CREATE TYPE app AS STRUCT(fun term, arg term);
 
 -- An enum type for the rules which can be applied by the SECD machine.
 -- Roughly corresponding to those presented by Danvy.
 CREATE TYPE rule AS ENUM('1', '2', '3', '4', '5', '6', '7');
 
-CREATE TYPE machine_state AS STRUCT(s stack, e env, c control, d dump);
-
 -- A single environment entry consists of:
 -- id: The environment's identifier
 -- name: The name of a variable
 -- val: The value to which this variable is bound in environment id
-CREATE TYPE env_entry AS STRUCT(id env, name var, val val);
-
-CREATE TYPE cte_type AS UNION(ms STRUCT(s stack, e env, c control, d dump), e env_entry);
 
 -- The self-referencing table terms holds all globally existing terms.
 -- invariant: After filling it with load_term, it doesn't change.
@@ -56,7 +24,7 @@ CREATE TYPE cte_type AS UNION(ms STRUCT(s stack, e env, c control, d dump), e en
 --      | Var Text        (Variable)
 --      | Lam Text Term   (Lambda abstraction with variable and body)
 --      | App Term Term   (Application with fun and arg)
-CREATE TABLE terms (id integer PRIMARY KEY, t UNION(lit int, var text, lam STRUCT(ide var, body integer), app STRUCT(fun integer, arg integer)));
+CREATE TABLE terms (id integer PRIMARY KEY, t UNION(lit int, var text, lam STRUCT(ide text, body integer), app STRUCT(fun integer, arg integer)));
 
 DROP SEQUENCE IF EXISTS env_keys;
 CREATE SEQUENCE env_keys START 1;
@@ -81,6 +49,7 @@ INSERT INTO terms VALUES
 INSERT INTO terms VALUES
   (nextval('term_keys'), UNION_VALUE(lit := 1));
 */
+-- ((λx.(λy.(x y))) (λx.x)) 1 --> 1
 INSERT INTO terms VALUES
   (nextval('term_keys'), UNION_VALUE(var := 'x')),
   (nextval('term_keys'), UNION_VALUE(var := 'y')),
