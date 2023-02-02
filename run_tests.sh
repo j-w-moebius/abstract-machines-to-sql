6dac60c61c6c25ba2e293f3d35d431f6d61c02b0
j-w-moebius <johann.moebius@gmx.net>
2023-02-02 11:23:56 +0100

    set up DuckDB tests

--- run_tests.sh
+++ run_tests.sh
@@ -5,22 +5,23 @@ test=$1
 case $test in 
    1) #Vanilla Postgres SECD
       psql -p $PSQL_PORT -q -f Vanilla-PSQL/SECD/secd.sql
-      psql -p $PSQL_PORT -o out.txt -c "SELECT id,n FROM root_terms AS _(id,t), LATERAL evaluate(t) AS r(_,n);"
+      psql -p $PSQL_PORT -o out.txt -c "SELECT id,val,n FROM root_terms AS _(id,t), LATERAL evaluate(t) AS r(val,n);"
       ;;
    2) # Vanilla Postgres Krivine
       psql -p $PSQL_PORT -q -f Vanilla-PSQL/Krivine/krivine.sql
-      psql -p $PSQL_PORT -o out.txt -c "SELECT id,n FROM root_terms AS _(id,t), LATERAL evaluate(t) AS r(_,n);"
+      psql -p $PSQL_PORT -o out.txt -c "SELECT id,val,n FROM root_terms AS _(id,t), LATERAL evaluate(t) AS r(val,n);"
       ;;
    3) #Hashtable Postgres SECD
-      psql -p $PSQL_PORT -o out.txt -q -c "\i Hashtables/SECD/secd.sql" -c "SELECT id,n FROM root_terms AS _(id,t), LATERAL evaluate(t) AS r(_,n);"
+      psql -p $PSQL_PORT -o out.txt -q -c "\i Hashtables/SECD/secd.sql" -c "SELECT id,val,n FROM root_terms AS _(id,t), LATERAL evaluate(t) AS r(val,n);"
       ;;
    4) #Hashtable Postgres Krivine
-      psql -p $PSQL_PORT -o out.txt -q -c "\i Hashtables/Krivine/krivine.sql" -c "SELECT id,n FROM root_terms AS _(id,t), LATERAL evaluate(t) AS r(_,n);"
+      psql -p $PSQL_PORT -o out.txt -q -c "\i Hashtables/Krivine/krivine.sql" -c "SELECT id,val,n FROM root_terms AS _(id,t), LATERAL evaluate(t) AS r(val,n);"
       ;;
-#   5) # DuckDB SECD (not working yet due to missing DuckDB features)
-#     psql -p $PSQL_PORT -q -f Vanilla-PSQL/SECD/secd.sql
-#     psql -p $PSQL_PORT -q -c "\copy (SELECT id, lit, var, (lam).ide, (lam).body, (app).fun, (app).arg FROM terms) TO 'terms.csv' CSV;"
-#    psql -p $PSQL_PORT -q -c "\copy (TABLE root_terms) TO 'root_terms.csv' CSV;"
-#     ./duckdb_cli -c ".output out.txt" -c ".read DuckDB/SECD/secd.sql" -c "SELECT id,evaluate(t) FROM root_terms AS _(id,t);"
-#     ;;
+   5) # DuckDB SECD (sed hack due to DuckDB forbidding the use of recursive CTEs in correlated subqueries)
+     psql -p $PSQL_PORT -q -f Vanilla-PSQL/SECD/secd.sql
+     psql -p $PSQL_PORT -q -c "\copy (SELECT id, lit, var, (lam).ide, (lam).body, (app).fun, (app).arg FROM terms) TO 'terms.csv' CSV;"
+     psql -p $PSQL_PORT -q -c "\copy (TABLE root_terms) TO 'root_terms.csv' CSV;"
+     cat root_terms.csv | sed -e 's/\(.*\),\(.*\)/SELECT \1 AS id,val,n FROM evaluate(\2) AS _(val,n);/g' > duckdb_commands.sql
+     ./duckdb_cli -c ".output out.txt" -c ".read DuckDB/SECD/secd.sql" -c ".read duckdb_commands.sql"
+     ;;
 esac
