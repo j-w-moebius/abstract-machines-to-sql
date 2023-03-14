c3149cc4ed0311f476bc1d6bbf7666e5266d005b
j-w-moebius <johann.moebius@gmx.net>
2023-03-15 00:12:29 +0100

    bundle columns id,name,val into new_env_entry in r (DuckDB SECD)

--- DuckDB/SECD/secd.sql
+++ DuckDB/SECD/secd.sql
@@ -49,51 +49,49 @@ CREATE OR REPLACE FUNCTION evaluate(t_init) AS TABLE (
       ),
 
       -- compute next machine state
-      -- id, name, val (optional): indicate (for rule 7) that a new 
-      --   environment has to be created by extending id with (name -> val)
-      step(finished,s,e,c,d,id,name,val) AS (
-        WITH one(finished,s,e,c,d,id,name,val) AS (
+      step(finished,s,e,c,d,new_env_entry) AS (
+        WITH one(finished,s,e,c,d,new_env_entry) AS (
         --1. Terminate computation
         SELECT true,
                ms.s, 
                ms.e, 
                ms.c, 
                ms.d, 
-               null,null,null
+               null
         FROM machine AS ms
         WHERE len(ms.s) = 1 
           AND len(ms.c) = 0 
           AND len(ms.d) = 0
         ),
 
-        two(finished,s,e,c,d,id,name,val) AS (
+        two(finished,s,e,c,d,new_env_entry) AS (
         --2. Return from function call
         SELECT false,
               ms.s || ms.d[1].s, 
               ms.d[1].e, 
               ms.d[1].c, 
               ms.d[2:], 
-              null,null,null
+              null
         FROM machine AS ms
         WHERE len(ms.s) = 1 
           AND len(ms.c) = 0
           AND len(ms.d) > 0
         ), 
 
-        three(finished,s,e,c,d,id,name,val) AS (
+        three(finished,s,e,c,d,new_env_entry) AS (
         --3. Push literal onto stack
         SELECT false,
               array_push_front(ms.s, t.lit),
               ms.e, 
               ms.c[2:], 
               ms.d, 
-              null,null,null
+              null
         FROM machine AS ms,
              term AS _(t)
         WHERE union_tag(t) = 'lit'
         ),
 
-        four(finished,s,e,c,d,id,name,val) AS (
+        four(finished,s,e,c,d,new_env_entry) AS (
         --4. Push variable value onto stack
         SELECT false,
                array_push_front(ms.s, (SELECT e.val
@@ -103,13 +101,13 @@ CREATE OR REPLACE FUNCTION evaluate(t_init) AS TABLE (
                 ms.e, 
                 ms.c[2:], 
                 ms.d, 
-                null,null,null
+                null
         FROM machine AS ms,
              term AS _(t)
         WHERE union_tag(t) = 'var'
         ),
 
-        five(finished,s,e,c,d,id,name,val) AS (
+        five(finished,s,e,c,d,new_env_entry) AS (
         --5. Push lambda abstraction onto stack as closure
         SELECT false,
               array_push_front(ms.s, {'v': t.lam.ide, 
@@ -118,26 +116,26 @@ CREATE OR REPLACE FUNCTION evaluate(t_init) AS TABLE (
               ms.e, 
               ms.c[2:], 
               ms.d, 
-              null, null,null
+              null
         FROM machine AS ms,
              term AS _(t)
         WHERE union_tag(t) = 'lam'
         ),
         
-        six(finished,s,e,c,d,id,name,val) AS (
+        six(finished,s,e,c,d,new_env_entry) AS (
         --6. Handle function application
         SELECT false,
               ms.s, 
               ms.e, 
               array_push_front(array_push_front(array_push_front(ms.c[2:], 'apply'::primitive), t.app.fun), t.app.arg), 
               ms.d, 
-              null,null,null
+              null
         FROM machine AS ms,
              term AS _(t)
         WHERE union_tag(t) = 'app'
         ), 
 
-        seven(finished,s,e,c,d,id,name,val) AS (
+        seven(finished,s,e,c,d,new_env_entry) AS (
         --7. Apply function
         SELECT false,
               [], 
@@ -146,62 +144,64 @@ CREATE OR REPLACE FUNCTION evaluate(t_init) AS TABLE (
               array_push_front(ms.d, {'s': ms.s[3:],
                 'e': ms.e,
                 'c': ms.c[2:]}), 
-              ms.s[1].c.e, ms.s[1].c.v, ms.s[2]
+              -- indicate that environment has to be created by extending id with (name -> val)
+              {'id': ms.s[1].c.e,
+               'name': ms.s[1].c.v, 
+               'val': ms.s[2]}
         FROM machine AS ms
         WHERE union_tag(ms.c[1]) = 'p'
           AND ms.c[1].p = 'apply'
         )
 
         -- the following cluster performs the union of the CTEs one, two, three, four, five, six, seven
-        -- its necessity arises from duckdb forbidding the use of UNIONs (and full outer joins) in recursive CTEs
+        -- its necessity arises from DuckDB forbidding the use of UNIONs (and full outer joins) in recursive CTEs
         SELECT COALESCE(one.finished, COALESCE(two.finished, COALESCE(three.finished, COALESCE(four.finished, COALESCE(five.finished, COALESCE(six.finished, seven.finished)))))),
                COALESCE(one.s, COALESCE(two.s, COALESCE(three.s, COALESCE(four.s, COALESCE(five.s, COALESCE(six.s, seven.s)))))),
                COALESCE(one.e, COALESCE(two.e, COALESCE(three.e, COALESCE(four.e, COALESCE(five.e, COALESCE(six.e, seven.e)))))),
                COALESCE(one.c, COALESCE(two.c, COALESCE(three.c, COALESCE(four.c, COALESCE(five.c, COALESCE(six.c, seven.c)))))),
                COALESCE(one.d, COALESCE(two.d, COALESCE(three.d, COALESCE(four.d, COALESCE(five.d, COALESCE(six.d, seven.d)))))),
-               COALESCE(one.id, COALESCE(two.id, COALESCE(three.id, COALESCE(four.id, COALESCE(five.id, COALESCE(six.id, seven.id)))))),
-               COALESCE(one.name, COALESCE(two.name, COALESCE(three.name, COALESCE(four.name, COALESCE(five.name, COALESCE(six.name, seven.name)))))),
-               COALESCE(one.val, COALESCE(two.val, COALESCE(three.val, COALESCE(four.val, COALESCE(five.val, COALESCE(six.val, seven.val))))))
+               COALESCE(one.new_env_entry, COALESCE(two.new_env_entry, COALESCE(three.new_env_entry, COALESCE(four.new_env_entry, COALESCE(five.new_env_entry, COALESCE(six.new_env_entry, seven.new_env_entry))))))
 
         FROM (SELECT (SELECT finished FROM one), (SELECT s FROM one), (SELECT e FROM one), (SELECT c FROM one), 
-             (SELECT d FROM one), (SELECT id FROM one), (SELECT name FROM one), (SELECT val FROM one)) 
-               AS one(finished,s,e,c,d,id,name,val),
+             (SELECT d FROM one), (SELECT new_env_entry FROM one)) 
+               AS one(finished,s,e,c,d,new_env_entry),
              (SELECT (SELECT finished FROM two), (SELECT s FROM two), (SELECT e FROM two), (SELECT c FROM two), 
-             (SELECT d FROM two), (SELECT id FROM two), (SELECT name FROM two), (SELECT val FROM two)) 
-               AS two(finished,s,e,c,d,id,name,val),
+             (SELECT d FROM two), (SELECT new_env_entry FROM two)) 
+               AS two(finished,s,e,c,d,new_env_entry),
              (SELECT (SELECT finished FROM three), (SELECT s FROM three), (SELECT e FROM three), (SELECT c FROM three), 
-             (SELECT d FROM three), (SELECT id FROM three), (SELECT name FROM three), (SELECT val FROM three)) 
-               AS three(finished,s,e,c,d,id,name,val),
+             (SELECT d FROM three), (SELECT new_env_entry FROM three)) 
+               AS three(finished,s,e,c,d,new_env_entry),
              (SELECT (SELECT finished FROM four), (SELECT s FROM four), (SELECT e FROM four), (SELECT c FROM four), 
-             (SELECT d FROM four), (SELECT id FROM four), (SELECT name FROM four), (SELECT val FROM four)) 
-               AS four(finished,s,e,c,d,id,name,val),
+             (SELECT d FROM four), (SELECT new_env_entry FROM four)) 
+               AS four(finished,s,e,c,d,new_env_entry),
              (SELECT (SELECT finished FROM five), (SELECT s FROM five), (SELECT e FROM five), (SELECT c FROM five), 
-             (SELECT d FROM five), (SELECT id FROM five), (SELECT name FROM five), (SELECT val FROM five)) 
-               AS five(finished,s,e,c,d,id,name,val),
+             (SELECT d FROM five), (SELECT new_env_entry FROM five)) 
+               AS five(finished,s,e,c,d,new_env_entry),
              (SELECT (SELECT finished FROM six), (SELECT s FROM six), (SELECT e FROM six), (SELECT c FROM six), 
-             (SELECT d FROM six), (SELECT id FROM six), (SELECT name FROM six), (SELECT val FROM six)) 
-               AS six(finished,s,e,c,d,id,name,val),
+             (SELECT d FROM six), (SELECT new_env_entry FROM six)) 
+               AS six(finished,s,e,c,d,new_env_entry),
              (SELECT (SELECT finished FROM seven), (SELECT s FROM seven), (SELECT e FROM seven), (SELECT c FROM seven), 
-             (SELECT d FROM seven), (SELECT id FROM seven), (SELECT name FROM seven), (SELECT val FROM seven)) 
-               AS seven(finished,s,e,c,d,id,name,val)
+             (SELECT d FROM seven), (SELECT new_env_entry FROM seven)) 
+               AS seven(finished,s,e,c,d,new_env_entry)
       ),
 
       --update the environments according to the rule applied by 'step'
       --each possible union_id corresponds to a UNION block in the PSQL query
+      -- (1: copy old envs, 2: extend env, 3: create new env)
       new_envs(id,name,val) AS (
         SELECT DISTINCT CASE union_id WHEN 1 THEN e.id
                                       WHEN 2 THEN currval('env_keys')   -- using s.e lead to strange behavior
                                       ELSE currval('env_keys') END,
                         CASE union_id WHEN 1 THEN e.name
-                                      WHEN 2 THEN s.name
+                                      WHEN 2 THEN s.new_env_entry.name
                                       ELSE e.name END,
                         CASE union_id WHEN 1 THEN e.val
-                                      WHEN 2 THEN s.val
+                                      WHEN 2 THEN s.new_env_entry.val
                                       ELSE e.val END
         FROM step AS s LEFT OUTER JOIN environment AS e ON true, (VALUES (1), (2), (3)) AS _(union_id)
         WHERE (union_id = 1 AND e.id NOT NULL) 
-          OR (union_id = 2 AND s.id NOT NULL)
-          OR (union_id = 3 AND s.id NOT NULL AND e.id = s.id AND e.name <> s.name)
+          OR (union_id = 2 AND s.new_env_entry NOT NULL)
+          OR (union_id = 3 AND s.new_env_entry NOT NULL AND e.id = s.new_env_entry.id AND e.name <> s.new_env_entry.name)
       )
 
       SELECT DISTINCT s.finished,
